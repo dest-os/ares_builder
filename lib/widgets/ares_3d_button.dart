6120ae3909384c156869780b9b1f5a43eca16114
dest-os <destosares@gmail.com>
2026-09-04 19:52:05 +0300

    Refactor Ares3DButton to use StatelessWidget

--- lib/widgets/ares_3d_button.dart
+++ lib/widgets/ares_3d_button.dart
@@ -1,76 +1,63 @@
 import 'package:flutter/material.dart';
-import '../theme/ares_theme.dart';
 
-class Ares3DButton extends StatefulWidget {
+class Ares3DButton extends StatelessWidget {
   final String text;
-  final VoidCallback onPressed;
-  final IconData? icon;
+  final IconData icon;
   final Color color;
   final Color shadowColor;
+  final VoidCallback onPressed;
 
   const Ares3DButton({
     super.key,
     required this.text,
+    required this.icon,
+    required this.color,
+    required this.shadowColor,
     required this.onPressed,
-    this.icon,
-    this.color = AresColors.primary,
-    this.shadowColor = AresColors.primaryDark,
   });
 
   @override
-  State<Ares3DButton> createState() => _Ares3DButtonState();
-}
-
-class _Ares3DButtonState extends State<Ares3DButton> {
-  bool _isPressed = false;
-
-  @override
   Widget build(BuildContext context) {
-    const double height = 52.0;
-    const double depth = 5.0;
-
-    return GestureDetector(
-      onTapDown: (_) => setState(() => _isPressed = true),
-      onTapUp: (_) {
-        setState(() => _isPressed = false);
-        widget.onPressed();
-      },
-      onTapCancel: () => setState(() => _isPressed = false),
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 60),
-        height: height,
-        margin: EdgeInsets.only(top: _isPressed ? depth : 0.0),
+    return Container(
+      height: 56,
+      decoration: BoxDecoration(
+        color: shadowColor,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 5),
         decoration: BoxDecoration(
-          color: widget.color,
-          borderRadius: BorderRadius.circular(14),
-          boxShadow: _isPressed
-              ? []
-              : [
-                  BoxShadow(
-                    color: widget.shadowColor,
-                    offset: const Offset(0, depth),
-                    blurRadius: 0,
-                  ),
-                ],
+          color: color,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [
+            BoxShadow(
+              color: color.withOpacity(0.4),
+              blurRadius: 8,
+              offset: const Offset(0, 2),
+            ),
+          ],
         ),
-        child: Center(
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              if (widget.icon != null) ...[
-                Icon(widget.icon, color: AresColors.textPrimary),
-                const SizedBox(width: 8),
-              ],
-              Text(
-                widget.text,
-                style: const TextStyle(
-                  color: AresColors.textPrimary,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 0.5,
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onPressed: onPressed,
+            borderRadius: BorderRadius.circular(16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(icon, color: Colors.white, size: 24),
+                const SizedBox(width: 10),
+                Text(
+                  text,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0.8,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
